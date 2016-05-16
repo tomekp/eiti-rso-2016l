8bd588924eebf0cc54c481dfef21b768fc3c51b5
Maczaj <daark.lith@gmail.com>
2016-05-16 18:25:40 +0200

    Test dostepnosci przy ubijaniu kolejnych node'ow, drobny refaktoring

--- tests/dlTest.sh
+++ tests/dlTest.sh
@@ -6,10 +6,10 @@ echo "-----------------------------------" >> temp_separator;
 fileId1=$(( ( RANDOM % 1000 )  + 1 ));
 fileId2=$(( ( RANDOM % 1000 )  + 1 ));
 fileId3=$(( ( RANDOM % 1000 )  + 1 ));
-#generate files ~ 500MB each
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 550000000 | head -n 1 >> temp_file1
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 550000000 | head -n 1 >> temp_file2
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 550000000 | head -n 1 >> temp_file3
+#generate files ~ 550MB each
+head -c550000K /dev/urandom >> temp_file1
+head -c550000K /dev/urandom >> temp_file2
+head -c550000K /dev/urandom >> temp_file3
 
 #TODO: zaladowac pliki, jak to zrobić? o ile dobrze rozumiem API to powinienem puścić PUTa z parametrem urlToDownload, ale coś nie banglało
 #curl -F ....
--- tests/setenv.sh
+++ tests/setenv.sh
@@ -1,1 +1,5 @@
 export RSO_URL="http://localhost:8080/";
+export RSO_REP_CNT=4;
+export RSO_MAX_NODES=8;
+export RSO_NODE_1="127.0.0.1";
+export RSO_NODE_2="127.0.0.1";
